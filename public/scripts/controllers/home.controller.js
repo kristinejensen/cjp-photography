594fa7cfb289e8804f91420a8efffa8d2c6b046a
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-05-11 10:40:58 -0500

    Working on angular ui slider

--- public/scripts/controllers/home.controller.js
+++ public/scripts/controllers/home.controller.js
@@ -1,5 +1,24 @@
 app.controller('HomeController', ['$http', '$location', function($http, $location){
   var self = this;
 
+  app.run(['Carousel', (Carousel) => {
+  Carousel.setOptions({
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    cssEase: 'ease',
+    dots: false,
 
-}]); // end 
+    easing: 'linear',
+    fade: false,
+    infinite: true,
+    initialSlide: 0,
+
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+  });
+}]);
+
+
+}]); // end
