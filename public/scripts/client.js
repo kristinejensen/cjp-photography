68e4419a7e754a4541360dfeb96ec0b20dfad12e
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-07-09 16:58:30 -0500

    Bringing in Angular Material

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,4 @@
-var app = angular.module('PhotoApp', ['ngRoute']);
+var app = angular.module('PhotoApp', ['ngRoute', 'ngMaterial']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
